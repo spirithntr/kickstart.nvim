bc8cdcd80ef537efe4283126e9a9a6cc3bd9ae43
John Solenya <spirithntr@gmail.com>
2023-09-06 16:39:39 +0400

    switch theme

--- init.lua
+++ init.lua
@@ -132,11 +132,12 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    -- Theme
+    'Mofiqul/vscode.nvim',
+    name = 'vscode',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'vscode'
     end,
   },
 
@@ -147,7 +148,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'vscode',
         component_separators = '|',
         section_separators = '',
       },
